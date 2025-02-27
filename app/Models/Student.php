72071b67ce522c3f5f9433c9eb68ff13daabfe70
Deo <deo.borg.g52247@mcast.edu.mt>
2025-02-27 17:58:11 +0000

    Routes setup (Section 2)

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    //to enable mass assignment
+    protected $fillable = ['name', 'email', 'phone', 'dob', 'college_id'];
+
+    //Section 7 ; A student can only be in one college
+    public function college(){
+        return $this->belongsTo(College::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//College routes
+Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index'); // list all colleges
+Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create'); //  form to add a new college
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit'); // form to update a college's details
+
+//Student routes
+Route::get('students', [StudentController::class, 'index'])->name('students.index'); // list all students
+Route::get('students/create', [StudentController::class, 'create'])->name('students.create'); // form to add a new student
+Route::get('students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit'); // form to update a student's details
+Route::get('students/{id}', [StudentController::class, 'destroy'])->name('students.destroy'); // delete a student record
